ffe97064c71a65dca37850f6796e18a9e0209796
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2019-09-21 05:24:31 -0700

    Add files via upload

--- power_tools_methods.py
+++ power_tools_methods.py
@@ -94,6 +94,60 @@ def string_to_list(string):
     return newlist
 
 
+thestring = """I like eggs on toast """
+
+def replace_words(string1,string2):
+    x = eval("thestring.replace('" + string1 + "','" + string2 +"')")
+    thestring = x
+    print(x)
+
+print(replace_words("eggs","cream of wheat"))
+
+
+
+
+
+#===========  file methods   ==========
+
+poem = """in the history of events
+          at san jose airport
+       """
+
+# write to file
+def write_to_file(name,words):
+    print("writing to file...",name)
+    file = eval("open('" + name + "', 'w+')")
+    file.write(words)
+    file.close()
+
+write_to_file('mypoem.text',poem)
+
+
+# append data to file
+def append_file(name,morewords):
+    file = eval("open('" + name + "', 'a+')")
+    file.write(morewords)
+    file.close()
+
+morewords = "just a little something new"
+
+append_file('mypoem.text',morewords)
+
+
+#read a file
+def read_file(name):
+    print("reading from file.....",name)
+    print("read file name =",name)
+    file = eval("open('" + name + "', 'r')")
+    if file.mode == 'r':
+        for line in file:
+            print(line)
+    file.close()
+
+
+read_file('mypoem.text')
+#read file line by line
+
 
 
 
